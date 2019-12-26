bc1bd65f08369b0ad936e08ca46551deb0efacf7
Ed Ezekiel <ed@ubiquisoft.com>
2019-12-26 12:24:52 -0600

    Use regex test

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -1,10 +1,11 @@
 function rot13(str) { // LBH QVQ VG!
     let decodedString = "";
-  
+    let letters = /[a-z]/i;
+
     // loop over each character in the str
     for (let char of str) {
       // if character is letter, convert character and add to decodedString
-      if (char.match(/[a-z]/i)) {
+      if (letters.test(char)) {
         let newCharCode = ((char.charCodeAt(0) - 65 + 13) % 26) + 65;
         decodedString += String.fromCharCode(newCharCode);
   
